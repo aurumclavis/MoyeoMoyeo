7c954bef5d03f67a614d5fd1c4bd5b5c310d56cc
zezaek <kejae214@gmail.com>
2022-07-09 00:44:25 +0900

    [U] events list, detail

--- src/components/units/events/detail/EventsDetail.Prsenter.tsx
+++ src/components/units/events/detail/EventsDetail.Prsenter.tsx
@@ -4,50 +4,43 @@ export default function EventsDetailUI() {
   return (
     <>
       <S.Wrapper>
-        <S.HeaderWrapper>
-          <S.HeaderTitle>행사명 행사명 행사명</S.HeaderTitle>
-          <S.HeaderCalendar>기간 2022.07.01 - 2022.07.29</S.HeaderCalendar>
-        </S.HeaderWrapper>
-        <S.BodyWrapper>
-          <S.MainWrapper>
-            <S.MainImg src="/배너이미지_행사1.png" />
-            <S.MainLeftWrapper>
-              <S.MainHeaderWrapper>
-                <S.MainTitle>이벤트타입 요약</S.MainTitle>
-                <S.MainHost>기관명</S.MainHost>
-              </S.MainHeaderWrapper>
-              <S.MainContents>
-                요약글 내용 요약글 내용 요약글 내용 요약글 내용 요약글 내용
-                요약글 내용 요약글 내용 요약글 내용 요약글 내용 요약글 내용
-                요약글 내용 요약글 내용 요약글 내용 요약글 내용 요약글 내용
-                요약글 내용 요약글 내용 요약글 내용 요약글 내용 요약글 내용
-                요약글 내용 요약글 내용 요약글 내용 요약글 내용 요약글 내용
-                요약글 내용 요약글 내용 요약글 내용 요약글 내용 요약글 내용
-                요약글 내용 요약글 내용 요약글 내용 요약글 내용 요약글 내용
-                요약글 내용 요약글 내용 요약글 내용 요약글 내용 요약글 내용
-                요약글 내용 요약글 내용 요약글 내용 요약글 내용요약글 내용
-                요약글 내용 요약글 내용 요약글 내용 요약글 내용 요약글 내용
-                요약글 내용 요약글 내용 요약글 내용 요약글 내용 요약글
-              </S.MainContents>
-            </S.MainLeftWrapper>
-          </S.MainWrapper>
-          <S.ContentsWrapper>
-            <S.MenuBtnWrapper>
-              <S.MenuBtn>상세보기</S.MenuBtn>
-              <S.MenuBtn>이벤트장소</S.MenuBtn>
-            </S.MenuBtnWrapper>
-            <S.Contents>
-              <S.ContentImg src="/배너이미지_행사1.png" />
-            </S.Contents>
-            <S.ContetsText></S.ContetsText>
-          </S.ContentsWrapper>
+        <S.MainHost>기관명 주최</S.MainHost>
+        <S.HeaderTitle>행사명 행사명 행사명</S.HeaderTitle>
+        <S.LineDiv />
+        <S.HeaderCalendar>2022.07.01 - 2022.07.29</S.HeaderCalendar>
+        <S.LineDiv />
 
-          <S.Footer>
-            <S.BtnRouter>관련 사이트 보러가기</S.BtnRouter>
-            <S.BtnRouterList>목록으로</S.BtnRouterList>
-            <S.BtnRouter>같이 갈 동행 구하기</S.BtnRouter>
-          </S.Footer>
-        </S.BodyWrapper>
+        <S.MainImg src="/배너이미지_행사1.png" />
+        <S.MarkerWrapper>
+          <S.MainTitle>이벤트타입 요약</S.MainTitle>
+          <S.MainContents>
+            요약글 내용 요약글 내용 요약글 내용 요약글 내용 요약글 내용 요약글
+            내용 요약글 내용 요약글 내용 요약글 내용 요약글 내용 요약글 내용
+            요약글 내용 요약글 내용 요약글 내용 요약글 내용 요약글 내용 요약글
+            내용 요약글 내용 요약글 내용 요약글 내용 요약글
+          </S.MainContents>
+        </S.MarkerWrapper>
+        <S.LineDiv />
+        <S.ContentsWrapper>
+          {/* <S.MenuBtnWrapper>
+            <S.MenuBtn>상세보기</S.MenuBtn>
+            <S.MenuBtn>이벤트장소</S.MenuBtn>
+          </S.MenuBtnWrapper> */}
+
+          <S.ContentImg src="/배너이미지_행사1.png" />
+          <S.ContetsText>
+            상세 설명이 들어갈 곳 입니다.상세 설명이 들어갈 곳 입니다.상세
+            설명이 들어갈 곳 입니다.상세 설명이 들어갈 곳 입니다.상세 설명이
+            들어갈 곳 입니다.상세 설명이 들어갈 곳 입니다.상세 설명이 들어갈 곳
+            입니다.상세 설명이 들어갈 곳 입니다.
+          </S.ContetsText>
+        </S.ContentsWrapper>
+        <S.LineDiv />
+        <S.Footer>
+          <S.BtnRouter>관련 사이트 보러가기</S.BtnRouter>
+          <S.BtnRouterList>목록으로</S.BtnRouterList>
+          <S.BtnRouter>같이 갈 동행 구하기</S.BtnRouter>
+        </S.Footer>
       </S.Wrapper>
     </>
   );
--- src/components/units/events/list/EventsList.Prsenter.tsx
+++ src/components/units/events/list/EventsList.Prsenter.tsx
@@ -24,55 +24,16 @@ export default function EventsListUI(props) {
         <S.ListWrapper>
           <S.ListImg src="/배너이미지_동행1.png" />
           <S.ContentsWrapper>
-            <S.ContentsTitleWrapper>
-              <S.ContentsTitle>축제 이름 축제 이름</S.ContentsTitle>
-              <S.ContentsPeriod>기간 yyyy-mm-dd - yyyy-mm-dd</S.ContentsPeriod>
-            </S.ContentsTitleWrapper>
-            <S.MarkerDetailWrapper>
-              <S.Marker>
-                요약글 요약글 요약글 요약글 요약글 요약글 요약글 요약글 요약글
-                요약글 요약글 요약글 요약글 요약글 요약글 요약글 요약글 요약글
-                요약글 요약글
-              </S.Marker>
-              <S.Btn>자세히 보러가기</S.Btn>
-            </S.MarkerDetailWrapper>
+            <S.ContentsTitle>축제 이름 축제 이름</S.ContentsTitle>
+            <S.ContentsPeriod>기간 yyyy-mm-dd - yyyy-mm-dd</S.ContentsPeriod>
+
+            <S.Marker>
+              요약글 요약글 요약글 요약글 요약글 요약글 요약글 요약글 요약글
+              요약글 요약글 요약글 요약글 요약글 요약글 요약글 요약글 요약글
+              요약글 요약글
+            </S.Marker>
           </S.ContentsWrapper>
-        </S.ListWrapper>
-        <S.LineListDiv />
-        <S.ListWrapper>
-          <S.ListImg src="/배너이미지_동행1.png" />
-          <S.ContentsWrapper>
-            <S.ContentsTitleWrapper>
-              <S.ContentsTitle>축제 이름 축제 이름</S.ContentsTitle>
-              <S.ContentsPeriod>기간 yyyy-mm-dd - yyyy-mm-dd</S.ContentsPeriod>
-            </S.ContentsTitleWrapper>
-            <S.MarkerDetailWrapper>
-              <S.Marker>
-                요약글 요약글 요약글 요약글 요약글 요약글 요약글 요약글 요약글
-                요약글 요약글 요약글 요약글 요약글 요약글 요약글 요약글 요약글
-                요약글 요약글
-              </S.Marker>
-              <S.Btn>자세히 보러가기</S.Btn>
-            </S.MarkerDetailWrapper>
-          </S.ContentsWrapper>
-        </S.ListWrapper>
-        <S.LineListDiv />
-        <S.ListWrapper>
-          <S.ListImg src="/배너이미지_동행1.png" />
-          <S.ContentsWrapper>
-            <S.ContentsTitleWrapper>
-              <S.ContentsTitle>축제 이름 축제 이름</S.ContentsTitle>
-              <S.ContentsPeriod>기간 yyyy-mm-dd - yyyy-mm-dd</S.ContentsPeriod>
-            </S.ContentsTitleWrapper>
-            <S.MarkerDetailWrapper>
-              <S.Marker>
-                요약글 요약글 요약글 요약글 요약글 요약글 요약글 요약글 요약글
-                요약글 요약글 요약글 요약글 요약글 요약글 요약글 요약글 요약글
-                요약글 요약글
-              </S.Marker>
-              <S.Btn>자세히 보러가기</S.Btn>
-            </S.MarkerDetailWrapper>
-          </S.ContentsWrapper>
+          <S.Btn>정보 더보기</S.Btn>
         </S.ListWrapper>
       </S.Wrapper>
     </>
