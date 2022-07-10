42a13b239e1b0c4117b5c18725c7607a5389d193
ad-astra3147 <AdAstra3147@gmail.com>
2022-07-10 16:54:26 +0900

    change layout

--- src/components/commons/layout/index.tsx
+++ src/components/commons/layout/index.tsx
@@ -10,8 +10,8 @@ interface ILayoutProps {
 }
 
 const Wrapper = styled.div`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
