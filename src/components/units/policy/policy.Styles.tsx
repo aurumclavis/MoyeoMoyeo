47ff2793da9ea8b52212d963585d612575498459
ad-astra3147 <AdAstra3147@gmail.com>
2022-07-10 12:54:25 +0900

    update FAQ

---
+++ src/components/units/policy/policy.Styles.tsx
@@ -0,0 +1,32 @@
+import styled from "@emotion/styled";
+
+export const OutWapper = styled.div`
+  width: 100%;
+  background-color: gray;
+  opacity: 50%;
+`;
+export const Wrapper = styled.div`
+  width: 500px;
+  height: 500px;
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
+`;
+export const Header = styled.div`
+  width: 100%;
+  height: 30px;
+  border-bottom: 1px solid #bdbdbd;
+  display: flex;
+  justify-content: space-between;
+`;
+export const Policy = styled.span`
+  font-weight: 700;
+`;
+export const CancelIcon = styled.img`
+  width: 20px;
+  height: 20px;
+`;
+export const PolicyContents = styled.div`
+  font-size: 0.625rem;
+  height: 300px;
+`;
