2421f823c04ca0cb88a41850a8a98a81930042e5
zezaek <kejae214@gmail.com>
2022-08-01 16:24:05 +0900

    [U] Layout Footer

--- src/components/commons/layout/footer/LayoutFooter.Presenter.tsx
+++ src/components/commons/layout/footer/LayoutFooter.Presenter.tsx
@@ -10,7 +10,7 @@ export default function LayoutFooterUI() {
               <S.LogoImg src="/icon/mainlogo2.png" />
               <S.ListContents>
                 <S.ListTitle>사업자 번호</S.ListTitle>
-                <S.ListText> 000-00-0000</S.ListText>
+                <S.ListText>000-00-0000</S.ListText>
               </S.ListContents>
               <S.ListContents>
                 <S.ListTitle>고객센터</S.ListTitle>
@@ -18,19 +18,18 @@ export default function LayoutFooterUI() {
               </S.ListContents>
               <S.ListContents>
                 <S.ListTitle>운영시간</S.ListTitle>
-                <S.ListText> AM 10:00 ~ PM 7:00</S.ListText>
+                <S.ListText>AM 10:00 ~ PM 7:00</S.ListText>
               </S.ListContents>
             </S.LeftListWrapper>
             <S.RightListWrapper>
               <S.ListContents>
                 <S.ListTitle>copyright</S.ListTitle>
-                <S.ListText> moyeomoyeo</S.ListText>
+                <S.ListText>moyeomoyeo</S.ListText>
               </S.ListContents>
               <S.ListContents>
                 <S.ListTitle>주소</S.ListTitle>
                 <S.ListText>
-                  서울특별시 구로구 구로동 188-25 지밸리 패스트파이브 구로점
-                  13층 1328호
+                  서울특별시 구로구 구로동 188-25 13층 1328호
                 </S.ListText>
               </S.ListContents>
             </S.RightListWrapper>
