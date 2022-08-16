faa2502bd345e6e7e4fdfd907ce046504058c2c5
zezaek <kejae214@gmail.com>
2022-08-16 16:26:25 +0900

    [U] app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,15 +7,15 @@ import ApolloSetting from "../src/components/commons/apollo";
 import { RecoilRoot } from "recoil";
 import Head from "next/head";
 function MyApp({ Component, pageProps }: AppProps) {
-  <Head>
-    <meta property="og:title" content="여기 모여? 모여!" />
-    <meta
-      property="og:description"
-      content="안녕하세요 함께 모여 만드는 모여모여입니다!"
-    />
-  </Head>;
   return (
     <RecoilRoot>
+      <Head>
+        <meta property="og:title" content="여기 모여? 모여!" />
+        <meta
+          property="og:description"
+          content="안녕하세요 함께 모여 만드는 모여모여입니다!"
+        />
+      </Head>
       <ApolloSetting>
         <Global styles={globalStyles} />
         <Layout>
